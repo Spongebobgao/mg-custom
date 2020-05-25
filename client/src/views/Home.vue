b9c4211de69a3188efad95b815f00c62b7da1ef7
Spongebobgao <spongebobgao@gmail.com>
2020-05-25 11:19:36 -0500

    setting up client and server

---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="mg-custom logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to MG-CUSTOM" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')//logging
+
+const app = express()
+
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send({
+    message: "hello"
+  })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server starts on ${port}`)
+})
