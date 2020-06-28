18dfa5528cfee267a8c439298baaa324877fccbe
Spongebobgao <spongebobgao@gmail.com>
2020-06-28 12:43:00 -0500

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -60,12 +60,7 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      component: Home,
-    },
-  ],
+  routes,
 })
 
 export default router
